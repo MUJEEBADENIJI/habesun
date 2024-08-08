16e9c8e9b97f5fe04de6b0ae5270d2a4acfe1f9b
unknown <adenijimujeeb8@gmail.com>
2024-08-08 21:18:33 +0100

    workingheader

--- copy.js
+++ copy.js
@@ -1,4 +1,4 @@
 const foot = 
- `<p id="copp">Copyright &copy <a href="/">HappyCode</a> 2024 - All Rights Reserved Nigga's Drip.</p>`;
+ `<p id="copp"><b> - Copyright &copy <a href="/">HappyCode</a> 2024 - All Rights Reserved Nigga's Drip. - </b></p>`;
 
  document.getElementById("copy").innerHTML= foot;
