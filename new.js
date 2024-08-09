060dbf95a7aba606a90acba3cf730d51d5c76015
unknown <adenijimujeeb8@gmail.com>
2024-08-09 12:39:25 +0100

    logo styling

--- new.js
+++ new.js
@@ -1,6 +1,6 @@
-const hed = `<ul>
+const hed = `<ul><b>
             <li><a href="index.html">Niggas</a> </li>
             <li><a href="boutique.html">Boutique</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="contact.html">Contact</a></li></b>
         </ul>`
 document.getElementById("road").innerHTML = hed;
