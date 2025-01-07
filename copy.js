a9256601f52dba4ad32e82532434b54d3b4e0970
mujeebadeniji <adenijimujeeb8@gmail.com>
2025-01-07 11:25:13 -0800

    HABESUN

--- copy.js
+++
@@ -1,3 +0,0 @@
-const foot = 
- `<p id="copp"><b> - Copyright &copy <a href="/">HappyCode</a> 2024 - All Rights Reserved Nigga's Drip. - </b> </p>`;
- document.getElementById("copy").innerHTML= foot;
--- new.js
+++
@@ -1,6 +0,0 @@
-const hed = `<ul><b>
-            <li><a href="index.html">Niggas</a> </li>
-            <li><a href="boutique.html">Boutique</a></li>
-            <li><a href="contact.html">Contact</a></li></b>
-        </ul>`
-document.getElementById("road").innerHTML = hed;
